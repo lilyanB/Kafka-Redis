feac104960ee518b4f1af46f9ccd612c125d66f8
lilyan <lilyan.bastien@gmail.com>
2022-03-28 16:51:59 +0200

    APIBinance

--- APIBinance.py
+++ APIBinance.py
@@ -14,27 +14,50 @@ SCHEMA = '"schema":{"name":"io.github.jaredpetersen.kafkaconnectredis.RedisSetCo
 def listAsset():
     r = requests.get(URL + "exchangeInfo")
     results = r.json()
+    ListAss = []
     for s in results['symbols']:
-        print(s['symbol'])
+        ListAss.append(s['symbol'])
+    return ListAss
 
 
 def getDepth(SYMBOLE,INFO):
   r = requests.get(URL + "depth", params=dict(symbol=SYMBOLE))
   results = r.json()
   frames = {side: pd.DataFrame(data=results[side], columns=["price", "quantity"], dtype=float) for side in [INFO]}
-  return(frames)
+  frames_list = [frames[side].assign(side=side) for side in frames]
+  data = pd.concat(frames_list, axis="index", ignore_index=True, sort=True)
+  return(data)
 
 
 def pushKafka(giveKey,giveValue):
     producer = KafkaProducer(bootstrap_servers='localhost:29092')
     for _ in range(2):
         producer.send('{"payload":{"key":"' + giveKey + '","value":"' + giveValue + '"},' + SCHEMA + '}')
-
     return True
+
+def all():
+    LISTE = listAsset()
+    FirstList = LISTE[0]
+    BIDS = getDepth(FirstList,"bids")
+    ASKS = getDepth(FirstList,"asks")
+    listBIDS = []
+    listASKS = []
+    for i in range(1,len(BIDS)):
+        valuee = BIDS["price"][i]
+        listBIDS.append(valuee)
+        pushKafka("price" + i,valuee)
+    for i in range(1, len(ASKS)):
+        valuee = ASKS["price"][i]
+        listASKS.append(valuee)
+        pushKafka("price" + i,valuee)
+
+
+
 
 
 if __name__ == '__main__':
-    print(listAsset())
-    print(getDepth("BTCBUSD","bids"))
-    print(getDepth("BTCBUSD","asks"))
-    pushKafka("employee", "1526")
+    listAsset()
+    #print(getDepth("BTCBUSD","bids"))
+    #print(getDepth("BTCBUSD","asks"))
+    #pushKafka("employee", "1526")
+    all()
