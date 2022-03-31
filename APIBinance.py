ba448550b419fd4d79b180ff870d6af5ed6bc31b
lilyan <lilyan.bastien@gmail.com>
2022-03-31 22:58:26 +0200

    affiche

--- APIBinance.py
+++ APIBinance.py
@@ -25,6 +25,7 @@ def getDepth(SYMBOLE,INFO):
   frames = {side: pd.DataFrame(data=results[side], columns=["price", "quantity"], dtype=float) for side in [INFO]}
   frames_list = [frames[side].assign(side=side) for side in frames]
   data = pd.concat(frames_list, axis="index", ignore_index=True, sort=True)
+  print(data)
   return(data)
 
 
@@ -35,21 +36,22 @@ def pushKafka(giveKey,giveValue):
 
 def all():
     LISTE = listAsset()
-    FirstList = LISTE[0]
-    BIDS = getDepth(FirstList,"bids")
-    ASKS = getDepth(FirstList,"asks")
+    print(LISTE)
+    Pair = input("Entrer une paire dans la liste que vous voyez si dessus :")
+    BIDS = getDepth(Pair,"bids")
+    ASKS = getDepth(Pair,"asks")
     listBIDS = []
     listASKS = []
     for i in range(1,len(BIDS)):
         valuee = BIDS["price"][i]
         listBIDS.append(valuee)
-        #print(listBIDS)
-        pushKafka("price" + i,valuee)
+        print(listBIDS)
+        #pushKafka("price" + i,valuee)
     for i in range(1, len(ASKS)):
         valuee = ASKS["price"][i]
         listASKS.append(valuee)
-        #print(listASKS)
-        pushKafka("price" + i,valuee)
+        print(listASKS)
+        #pushKafka("price" + i,valuee)
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -2,17 +2,23 @@
 
 Lilyan BASTIEN 2022
 
-## Usage of the scripts
+### Usage of the scripts
 ```bash
 cd Kafka-Redis
 ```
 
-## Setup all Docker containers
+### Setup all Docker containers
 ```bash
 docker-compose up
 ```
 
-## Install the Redis Kafa Connect connector from HTTP command
+### Install the Redis Kafa Connect connector from HTTP command
 ```bash
 curl -s -X POST -H 'Content-Type: application/json' --data @redis-sink-config.json http://localhost:8083/connectors
 ```
+### Run APIBinance.py
+With this files, you will push the Bitcoin price during 5min
+You can change "time" depending on your needs
+
+### Run Affiche.py
+When you want to have the Price, you can run this files
